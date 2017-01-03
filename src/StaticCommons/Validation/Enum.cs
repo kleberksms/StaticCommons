b96c2142bb0b0e4dc486af04e69b99d25b5a9e1d
Kleber <sistemas.kleber@bellinatiperez.com.br>
2017-01-03 17:11:30 -0200

    exist from description enum

---
+++ src/StaticCommons/Validation/Enum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace StaticCommons.Validation
+{
+    public static class Enum
+    {
+        public static bool ExistFromDescription<T>(string description)
+        {
+            var type = typeof(T);
+            if (!type.GetTypeInfo().IsEnum) throw new InvalidOperationException();
+            foreach (var field in type.GetFields())
+            {
+                var attribute = field.GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute;
+                if (attribute != null)
+                {
+                    if (attribute.Description == description)
+                        return true;
+                }
+                else
+                {
+                    if (field.Name == description)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
