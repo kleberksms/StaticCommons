7a6f0cb4eb34492c425129fd8d01ec6f5e42e1ee
Kleber <sistemas.kleber@bellinatiperez.com.br>
2017-02-01 10:28:20 -0200

    add email validation

---
+++ src/StaticCommons/Validation/Email.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StaticCommons.Validation
+{
+    public static class Email
+    {
+        public static bool IsValid(string email)
+        {
+            const string pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+            var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            return regex.IsMatch(email);
+        }
+    }
+}
