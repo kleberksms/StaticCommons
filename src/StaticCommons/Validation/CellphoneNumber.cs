5089c8a504f1cf746e0f1cfb5f94290db56382eb
Kleber <sistemas.kleber@bellinatiperez.com.br>
2017-02-24 11:18:20 -0300

    validate cellphone

---
+++ src/StaticCommons/Validation/CellphoneNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StaticCommons.Validation
+{
+    public static class CellphoneNumber
+    {
+        public static bool IsValid(Country country, string number)
+        {
+            if (string.IsNullOrEmpty(number))
+            {
+                return false;
+            }
+            if (country.Equals(Country.Br))
+            {
+                return Convert.ToInt32(number.Substring(0, 1)) >= 6;
+            }
+            return false;
+        }
+    }
+
+    public enum Country
+    {
+        Br
+    }
+}
