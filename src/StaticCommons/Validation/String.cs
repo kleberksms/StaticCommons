0ceb056de5a913c9a474ae3aa71ad45d886576c0
Kleber <sistemas.kleber@bellinatiperez.com.br>
2018-05-17 13:40:32 -0300

    Update

--- src/StaticCommons/Validation/String.cs
+++ src/StaticCommons/Validation/String.cs
@@ -7,7 +7,20 @@ namespace StaticCommons.Validation
 {
     public static class String
     {
-        public static bool IsTextMatches(string text, string data, string source)
+        public static bool IsTextMatches(string text, string data)
+        {
+            var result = false;
+            foreach (var s in text.Slugfy().Split(new[] { "-" }, StringSplitOptions.None))
+            {
+                if (Regex.Matches(s, data).Any())
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public static bool ContainsTextMatches(this string text, string data)
         {
             var result = false;
             foreach (var s in text.Slugfy().Split(new[] { "-" }, StringSplitOptions.None))
