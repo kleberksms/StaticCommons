45ac84c4e7ada12143cdc8eb7d71070fd4a25ad8
Kleber <sistemas.kleber@bellinatiperez.com.br>
2018-05-17 12:01:36 -0300

    Is Text Matches

---
+++ src/StaticCommons/Validation/String.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using StaticCommons.Filter.String;
+
+namespace StaticCommons.Validation
+{
+    public static class String
+    {
+        public static bool IsTextMatches(string text, string data, string source)
+        {
+            var result = false;
+            foreach (var s in text.Slugfy().Split(new[] { "-" }, StringSplitOptions.None))
+            {
+                if (Regex.Matches(s, data).Any())
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
